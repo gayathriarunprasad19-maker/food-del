3c8e2087d55701a34f892079d861a5157f642c60
gayathri <gayathriarunprasad19@gmail.com>
2026-02-12 10:41:04 +0530

    Fixed CORS production issue

--- food-del/frontend/src/Context/StoreContext.jsx
+++ food-del/frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = ({ children }) => {
 
   // ✅ BACKEND BASE URL (REQUIRED FOR LOGIN/SIGNUP)
-  const url = "http://localhost:4000";
+  const url = "https://food-del-backend-0w4i.onrender.com"
 
   // ✅ STATIC FOOD DATA
   const [food_list] = useState(foodData);
