ad1b732f8004f2be9acca9611198f3a6b36514d6
gayathri <gayathriarunprasad19@gmail.com>
2026-02-13 11:22:56 +0530

    Allowed token header in CORS

--- food-del/backend/server.js
+++ food-del/backend/server.js
@@ -26,7 +26,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization","token"]
 }));
 
 // ✅ HANDLE PREFLIGHT REQUESTS
